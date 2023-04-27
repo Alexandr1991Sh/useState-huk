d5e2c3f76698726292cb6c0c3da6dbd73cd2c6f9
Alexandr1991Sh <an1991sh@yandex.ru>
2023-04-27 08:04:24 +0300

    UseState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // let a = 1
+    let [a, setA] = useState(1)
+    const increase = () => {
+        setA(++a)
+        console.log(a++)
+    }
+
+    const del = () => {
+        setA(a = 0)
+    }
+
+    return (
+        <div className="App">
+            <button onClick={increase}>Push My</button>
+            <button onClick={del}>0</button>
+            <div>
+                {a}
+            </div>
+        </div>
+    );
 }
 
 export default App;
